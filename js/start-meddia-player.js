8c9d5a1ddc6f664c47e96fda11532f7a478a5a42
Diego Brocanelli <diegod2@msn.com>
2017-11-11 13:17:30 -0200

    reoção de material. Inclusão de mensagem na home informando o site oficial do canal.

--- js/start-meddia-player.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('video,audio').mediaelementplayer({
-        features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],
-        alwaysShowControls: false,
-        enableKeyboard: false
-    });
-});
